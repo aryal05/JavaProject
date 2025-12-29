0a2731988ddca644562dc8afd09a051deacfeba5
aryal05 <aryal.rajat05@gmail.com>
2025-12-29 09:45:12 +0545

    adding getter methods for name and age

--- src/WorkShop7/Student.java
+++ src/WorkShop7/Student.java
@@ -1,5 +1,24 @@
 package WorkShop7;
-
 public class Student {
 
+    private String name;
+    private int age;
+    
+    public Student(String name, int age) {
+    	this.name=name;
+    	this.age=age;
+    }
+    	
+    public String getName() {
+    	return name;
+    	
+    }
+    public int getAge() {
+    	return age;
+   
+    	
+    }
 }
+
+
+
